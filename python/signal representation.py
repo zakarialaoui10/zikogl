9cbd78751849596b5bff1d84b3ad95b24b71cdd6
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2021-06-30 22:43:27 +0100

    Update signal representation.py

--- python/signal representation.py
+++ python/signal representation.py
@@ -1,18 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
-t=np.arange(-10,10,1)
-def rampe(t):
+t=np.arange(-10,10,0.25)
+def rampe(t,t0):
     x=t.copy()
-    x[x<0]=0
+    x=x-t0;
+    x[x<0]=0;
     return x
 def sign(x):
     return np.sign(x)
-def echelon(t):
-    return np.ones(len(t))
-plt.axis([-10,10,0,10])
-plt.plot(t,rampe(t))
-plt.show()
-plt.plot(t,sign(t))
-plt.show()
-plt.stem(t,echelon(t))
-plt.show()
+def echelon(t,t0=0):
+    return np.where(t>=t0,1,0)
+def rect(t,centre=0,ecart=1):
+    return np.where((t>=centre-ecart/2) & (t<=centre+ecart/2),1,0)
+def sinc(t):
+    return np.sinc(t)
