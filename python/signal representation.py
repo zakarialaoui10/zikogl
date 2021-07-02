7b2126e1bd92a40f94eb054ef5c61915719a0ad2
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2021-07-02 01:08:36 +0100

    Update signal representation.py

--- python/signal representation.py
+++ python/signal representation.py
@@ -8,7 +8,7 @@ def rampe(t,t0):
     return x
 def sign(x):
     return np.sign(x)
-def echelon(t,t0=0):
+def step(t,t0=0):
     return np.where(t>=t0,1,0)
 def rect(t,centre=0,ecart=1):
     return np.where((t>=centre-ecart/2) & (t<=centre+ecart/2),1,0)
