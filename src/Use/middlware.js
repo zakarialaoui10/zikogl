1dfa9bcce5ad873445e0c7bae5db5cba72547613
zakarialaoui10 <zakarialaoui10@gmail.com>
2024-10-13 15:45:06 +0100

    upgrade

--- src/Use/middlware.js
+++ src/Use/middlware.js
@@ -1,6 +1,6 @@
 const useCoordinates=(e,scene,render=true)=>{
-    scene.cache.watch.intersection.pointer.x = ( e.mx / window.innerWidth ) * 2 - 1;
-	scene.cache.watch.intersection.pointer.y = - ( e.my / window.innerHeight ) * 2 + 1;
+    scene.cache.watch.intersection.pointer.x = ( e.mx / globalThis?.innerWidth ) * 2 - 1;
+	scene.cache.watch.intersection.pointer.y = - ( e.my / globalThis?.innerHeight ) * 2 + 1;
     if(render)scene.renderGl()
 }
 export{
